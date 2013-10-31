3a8688602e3ffde4a69df979ca6d0cfe8905fba1
Nobuyoshi Nakada <nobu@ruby-lang.org>
2013-11-01 02:56:53 +0900

    weakref.rb: anonymous class wrapper
    * lib/weakref.rb: make wrapper an anonymous class instead of singleton
      object, to resolve chained weakrefs.

--- test/test_weakref.rb
+++ test/test_weakref.rb
@@ -2,17 +2,53 @@ require 'test/unit'
 require 'weakref'
 require_relative './ruby/envutil'
 
-class TestWeakRef < Test::Unit::TestCase
+module TestWeakRef
+  class Delegation < Test::Unit::TestCase
+    include TestWeakRef
+
+    NEW_WEAKREF = 'WeakRef.new'
+    def new_weakref(obj)
+      EnvUtil.suppress_warning do
+        WeakRef.new(obj)
+      end
+    end
+
+    def get(w)
+      w
+    end
+
+    def assert_no_ref(&block)
+      assert_raise(WeakRef::RefError, &block)
+    end
+  end
+
+  class NonDelegation < Test::Unit::TestCase
+    include TestWeakRef
+
+    NEW_WEAKREF = 'WeakRef.[]'
+    def new_weakref(obj)
+      WeakRef[obj]
+    end
+
+    def get(w)
+      w.get
+    end
+
+    def assert_no_ref(&block)
+      assert_nil(yield)
+    end
+  end
+
   def make_weakref(level = 10)
     obj = Object.new
     str = obj.to_s
-    level.times {obj = WeakRef.new(obj)}
-    return WeakRef.new(obj), str
+    level.times {obj = new_weakref(obj)}
+    return new_weakref(obj), str
   end
 
   def test_ref
     weak, str = make_weakref
-    assert_equal(str, weak.to_s)
+    assert_equal(str, get(weak).to_s)
   end
 
   def test_recycled
@@ -21,7 +57,7 @@ class TestWeakRef < Test::Unit::TestCase
     assert_predicate(weak, :weakref_alive?)
     ObjectSpace.garbage_collect
     ObjectSpace.garbage_collect
-    assert_raise(WeakRef::RefError) {weak.to_s}
+    assert_no_ref {o = get(weak) and o.to_s}
     assert_not_predicate(weak, :weakref_alive?)
   end
 
@@ -31,13 +67,13 @@ class TestWeakRef < Test::Unit::TestCase
     3.times do
       obj = Object.new
       def obj.foo; end
-      weakrefs << WeakRef.new(obj)
+      weakrefs << new_weakref(obj)
       ObjectSpace.garbage_collect
     end
     assert_nothing_raised(NoMethodError, bug7304) {
       weakrefs.each do |weak|
         begin
-          weak.foo
+          o = get(weak) and o.foo
         rescue WeakRef::RefError
         end
       end
@@ -46,11 +82,11 @@ class TestWeakRef < Test::Unit::TestCase
 
   def test_weakref_finalize
     bug7304 = '[ruby-core:49044]'
-    assert_normal_exit %q{
+    assert_normal_exit %Q{
       require 'weakref'
       obj = Object.new
       3.times do
-        WeakRef.new(obj)
+        #{self.class::NEW_WEAKREF}(obj)
         ObjectSpace.garbage_collect
       end
     }, bug7304
